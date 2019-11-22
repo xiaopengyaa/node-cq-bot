ce7db0d110a59854af63e576ce1b8f4e336d1123
xiaopengyaa <857763476@qq.com>
2019-11-22 18:20:00 +0800

    feat: 增加除夕倒计时

--- src/js/schedule/one.js
+++ src/js/schedule/one.js
@@ -1,4 +1,5 @@
 const cheerio = require('cheerio')
+const moment = require('moment')
 const { oneApi } = require('../../api')
 
 const one = {
@@ -6,9 +7,15 @@ const one = {
     const data = await oneApi.getOne()
     const $ = cheerio.load(data)
     const text = $('#carousel-one .fp-one-cita a').eq(0).text()
-    console.log(text)
-    return [`【每日一句】\r\n${text}`]
+    const countDown = moment('2020-01-24').diff(moment().format('YYYY-MM-DD'), 'days') // 除夕倒计时
+    let festivalText = ''
+    if (countDown > 0) {
+      festivalText = `\r\n\r\n温馨提示：离【除夕】还有${countDown}天[CQ:face,id=175]`
+    } else if (countDown === 0) {
+      festivalText = '\r\n\r\n温馨提示：祝大家除夕快乐呀[CQ:face,id=175]'
+    }
+    console.log('每日一句：', text)
+    return [`【每日一句】\r\n\r\n${text}${festivalText}`]
   }
 }
-
 module.exports = one
